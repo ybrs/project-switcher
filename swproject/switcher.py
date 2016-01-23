b80f2d6cb3f7c8816a13f3f9a93f291a0d36856e
ybrs <aybars.badur@gmail.com>
2016-01-23 14:41:43 +0200

    added chrome

--- swproject/switcher.py
+++ swproject/switcher.py
@@ -1,6 +1,52 @@
 import os
 import sys
 import yaml
+import subprocess
+
+
+def run_chrome(url):
+    myscript = """
+        osascript <<-EOF
+            tell application "Google Chrome"
+                activate
+                open location "{url}"
+                delay 1
+                activate
+            end tell
+        EOF
+    """.format(url=url)
+    print myscript
+    proc = subprocess.Popen(myscript,
+                     shell=True,
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+    (output, error) = proc.communicate()
+    print output
+
+def run_iterm(lines):
+    ll = [ln for ln in lines if ln.strip()]
+    myscript = """
+        osascript <<-EOF
+        tell application "iTerm"
+            activate
+            make new terminal
+            tell the current terminal
+                activate current session
+                launch session "Default Session"
+                tell the last session
+                    write text "{lines}"
+                end tell
+            end tell
+        end tell
+        EOF
+    """.format(lines=' && '.join(ll))
+    print myscript
+    proc = subprocess.Popen(myscript,
+                     shell=True,
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+    (output, error) = proc.communicate()
+    print output
 
 def run(fname, projectname):
     f = open(fname)
@@ -14,9 +60,9 @@ def run(fname, projectname):
         for k, v in cmd.items():
             if k == 'iterm':
                 lines = v.split("\n")
-                for ln in lines:
-                    if ln:
-                        print ">>>", ln
+                run_iterm(lines)
+            if k == 'chrome':
+                run_chrome(v['open'])
 
 
 if __name__ == '__main__':
